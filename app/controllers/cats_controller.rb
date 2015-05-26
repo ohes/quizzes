2e07e1909a37d43787acff5de77b5f7b710bab96
ohes <estheroh@gmail.com>
2015-05-26 01:59:39 +0000

    allowed users to create new cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -7,4 +7,16 @@ class CatsController < ApplicationController
 		@cat = Cat.new
 	end
 
+	def create
+		Cat.create(cat_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def cat_params
+		params.require(:cat).permit(:name, :description, :birthday)
+	end
+
+
 end
